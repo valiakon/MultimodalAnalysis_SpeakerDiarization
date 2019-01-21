6a1896b5d1bf1924c4bc8761e568c292ac3c1c3a
elvoulga <elvoulga25@gmail.com>
2019-01-22 00:01:16 +0200

    Clustering Function

--- clustering_face.py
+++ clustering_face.py
@@ -5,14 +5,17 @@ from sklearn.cluster import SpectralClustering
 
 if __name__ == '__main__':
 
+def FaceClustering():
 
 	feat = pd.read_csv('visual_features_HOG.csv', delimiter=',', header=None)
 	
-	kmeans = KMeans(n_clusters=3, random_state=0).fit_predict(feat)
-	print "kmeans", kmeans
+#	kmeans = KMeans(n_clusters=3, random_state=0).fit_predict(feat)
+#	print "kmeans", kmeans
 	
-	agglo =  AgglomerativeClustering(n_clusters = 3,  affinity='euclidean').fit_predict(feat)
-	print "agglom", agglo
+#	agglo =  AgglomerativeClustering(n_clusters = 3,  affinity='euclidean').fit_predict(feat)
+#	print "agglom", agglo
 
 	spect = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit_predict(feat)
 	print "spect", spect
+
+    return metrics.silhouette_score(feat, spect, metric='euclidean')
