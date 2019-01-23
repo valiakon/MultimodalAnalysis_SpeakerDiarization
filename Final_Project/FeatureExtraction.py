9c1fdcea361ffa53e3ebafab2497d35cb2076d45
valiakon <valiakonstantopoulou.vk@gmail.com>
2019-01-23 21:07:00 +0200

    rounding decimals for audio features

--- Final_Project/FeatureExtraction.py
+++ Final_Project/FeatureExtraction.py
@@ -8,7 +8,20 @@ from SpeechFeatures import StereoToMono
 from SpeechFeatures import ExtractFeatures
 from MouthDetection import mouthDetection
 from FaceDetection import face_detection
-from FaceDetection import write_csv
+import csv
+
+
+
+def write_csv(features, filename):
+	feat_to_csv_final = []
+	with open(filename, 'a') as feat:
+		wr = csv.writer(feat, delimiter=',')
+		for i in features:
+			feat_to_csv = []
+			print i[0]
+			for j in range(len(i[0])):
+				feat_to_csv.append(float('%.5f'%(i[0][j])))
+			wr.writerow(feat_to_csv)
 
 def main():
     path = '/home/valia/Desktop/videos1/'   #'/Users/thanasiskaridis/Desktop/multimodal_/full_videos/'
@@ -32,6 +45,7 @@ def main():
             for sec_features in face_feats:
                 face_features.append(sec_features)
     final_audio_features = pd.DataFrame(np.row_stack(audio_features))
+	final_audio_features = final_audio_features.round(5)
     final_audio_features.to_csv('final_audio_features.csv', sep = ',', mode= 'a', header=None, index=False)
     final_mouth_features = pd.DataFrame(np.row_stack(mouth_features))
     final_mouth_features.to_csv('final_mouth_features.csv', sep = ',', mode= 'a', header=None, index=False)
