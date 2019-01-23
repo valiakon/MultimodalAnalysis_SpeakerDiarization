97f5c5a648e6cc61000f5f0ace554e172d0eee9a
elvoulga <elvoulga25@gmail.com>
2019-01-23 23:26:18 +0200

    Clusterings Changes

--- Final_Project/Clusterings.py
+++ Final_Project/Clusterings.py
@@ -16,56 +16,56 @@ def SpeeechClustering():
     manifold_15Da = manifold15.fit_transform(feats)
     speech_feats = pd.DataFrame(manifold_15Da)
 
-    hc = AgglomerativeClustering(n_clusters=3, compute_full_tree='False', affinity = 'euclidean', linkage = 'ward')
+    hc = AgglomerativeClustering(n_clusters=3, affinity = 'euclidean', linkage = 'ward')
     labels = hc.fit_predict(speech_feats)
 
-#	kmeans = KMeans(n_clusters=3, random_state=0).fit_predict(manifold_15D)
-#	print "kmeans", kmeans
+#    kmeans = KMeans(n_clusters=3, random_state=0).fit_predict(speech_feats)
+#    print "kmeans", kmeans
 	
-#	spect = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit_predict(manifold_15D)
-#	print "Spectral Clustering: ", spect
+#    spect = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit_predict(speech_feats)
+#    print "Spectral Clustering: ", spect
 
     return metrics.silhouette_score(speech_feats, labels, metric='euclidean')
 
 
 def FaceClustering():
 
-	face_feats = pd.read_csv('final_face_features.csv', delimiter=',', header=None)
+    face_feats = pd.read_csv('final_face_features.csv', delimiter=',', header=None)
 	
-#	kmeans = KMeans(n_clusters=3, random_state=0).fit_predict(feat)
-#	print "kmeans", kmeans
+#    kmeans = KMeans(n_clusters=3, random_state=0).fit_predict(face_feats)
+#    print "kmeans", kmeans
 	
-#	agglo =  AgglomerativeClustering(n_clusters = 3,  affinity='euclidean').fit_predict(feat)
-#	print "agglom", agglo
+#    agglo =  AgglomerativeClustering(n_clusters = 3,  affinity='euclidean').fit_predict(face_feats)
+#    print "agglom", agglo
 
-	spect = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit_predict(face_feats)
-#	print "Spectral Clustering: ", spect
+    spect = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit_predict(face_feats)
+#    print "Spectral Clustering: ", spect
 
-	return metrics.silhouette_score(face_feats, spect, metric='euclidean')
+    return metrics.silhouette_score(face_feats, spect, metric='euclidean')
 
 
 def FaceMouthClustering():
 
-	face_feats = pd.read_csv('final_face_features.csv', delimiter=',', header=None)
+    face_feats = pd.read_csv('final_face_features.csv', delimiter=',', header=None)
     mouth_feats = pd.read_csv('final_mouth_features.csv', delimiter=',', header=None)
 
     both_feats = pd.concat([face_feats, mouth_feats], axis=1, ignore_index=True)
 	
-#	kmeans = KMeans(n_clusters=3, random_state=0).fit_predict(feat)
-#	print "kmeans", kmeans
+#    kmeans = KMeans(n_clusters=3, random_state=0).fit_predict(both_feats)
+#    print "kmeans", kmeans
 	
-#	agglo =  AgglomerativeClustering(n_clusters = 3,  affinity='euclidean').fit_predict(feat)
-#	print "agglom", agglo
+#    agglo =  AgglomerativeClustering(n_clusters = 3,  affinity='euclidean').fit_predict(both_feats)
+#    print "agglom", agglo
 
-	spect = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit_predict(both_feats)
-#	print "Spectral Clustering: ", spect
+    spect = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit_predict(both_feats)
+#    print "Spectral Clustering: ", spect
 
-	return metrics.silhouette_score(both_feats, spect, metric='euclidean')
+    return metrics.silhouette_score(both_feats, spect, metric='euclidean')
 
 
 def AllFeaturesClustering():
 
-	face_feats = pd.read_csv('final_face_features.csv', delimiter=',', header=None)
+    face_feats = pd.read_csv('final_face_features.csv', delimiter=',', header=None)
     mouth_feats = pd.read_csv('final_mouth_features.csv', delimiter=',', header=None)
     sp_feats = pd.read_csv("final_audio_features.csv", sep = ',', header=None)
 
@@ -75,13 +75,13 @@ def AllFeaturesClustering():
 
     all_feats = pd.concat([face_feats, mouth_feats, speech_feats], axis=1, ignore_index=True)
 	
-#	kmeans = KMeans(n_clusters=3, random_state=0).fit_predict(all_feats)
-#	print "kmeans", kmeans
+#    kmeans = KMeans(n_clusters=3, random_state=0).fit_predict(all_feats)
+#    print "kmeans", kmeans
 	
-#	agglo =  AgglomerativeClustering(n_clusters = 3,  affinity='euclidean').fit_predict(all_feats)
-#	print "agglom", agglo
+#    agglo =  AgglomerativeClustering(n_clusters = 3,  affinity='euclidean').fit_predict(all_feats)
+#    print "agglom", agglo
 
-	spect = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit_predict(all_feats)
-#	print "Spectral Clustering: ", spect
+    spect = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit_predict(all_feats)
+#    print "Spectral Clustering: ", spect
 
-	return metrics.silhouette_score(all_feats, spect, metric='euclidean')
+    return metrics.silhouette_score(all_feats, spect, metric='euclidean')
