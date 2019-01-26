50243f0bfd5d0a99c9e717707011e5346fe6ceb9
elvoulga <elvoulga25@gmail.com>
2019-01-26 22:38:23 +0200

    Refactor code for Clustering

--- Final_Project/TotalClusterings.py
+++ Final_Project/TotalClusterings.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn import metrics
 from sklearn.cluster import KMeans
+from sklearn import preprocessing
 
 
 def main():
@@ -12,24 +13,32 @@ def main():
     path = '/home/eleni/Desktop/Multimodal/Results/'
     files = os.listdir(path)
     for f in files:
+        print f
+        print path+f
         features = pd.read_csv(path+f, sep = ',', header=None, index_col=False)
+#        print features
         features = features[features.columns[:-2]]
-        kmeans = KMeans(n_clusters=3, random_state=0).fit(features)
-        labels = kmeans.predict(features)
-#        print "Sillhouette", metrics.silhouette_score(features, labels, metric='euclidean')
-        write_line = []
+
+        x = features.values
+        scaler = preprocessing.MinMaxScaler()
+        x_scaled = scaler.fit_transform(x)
+        features = pd.DataFrame(x_scaled)
+
+        kmeans = KMeans(n_clusters=3, random_state=0)
+        labels = kmeans.fit_predict(features)
+        print "Sillhouette", metrics.silhouette_score(features, labels, metric='euclidean')
         name_list = [f[:-4]+"CL0", f[:-4]+"CL1", f[:-4]+"CL2"]   
-         
+        
         indexing = [[i for i, e in enumerate(labels) if e == 0], [i for i, e in enumerate(labels) if e == 1], [i for i, e in enumerate(labels) if e == 2]]
-        df = pd.DataFrame(indexing)
+        df = pd.DataFrame({0: indexing})
         df['Origin'] = name_list
         
         if f[:5] == "audio":
-            df.to_csv('/home/eleni/Desktop/Multimodal/AllResults/all_audio_clustering_results.csv', sep = ',', index=False, header=False)
+            df.to_csv('/home/eleni/Desktop/Multimodal/AllResults/super_audio_clustering_results.csv', sep = ',', index=False, header=False)
         elif f[:6] == "visual":
-            df.to_csv('/home/eleni/Desktop/Multimodal/AllResults/all_visual_clustering_results.csv', sep = ',', index=False, header=False)
+            df.to_csv('/home/eleni/Desktop/Multimodal/AllResults/super_visual_clustering_results.csv', sep = ',', index=False, header=False)
         else:
-            df.to_csv('/home/eleni/Desktop/Multimodal/AllResults/all_all_clustering_results.csv', sep = ',', index=False, header=False)
+            df.to_csv('/home/eleni/Desktop/Multimodal/AllResults/super_all_clustering_results.csv', sep = ',', index=False, header=False)
            
 
  
